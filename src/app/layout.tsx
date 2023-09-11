a8158e82fd82fac3c25704c4223ea3b291dd7085
ennea8 <ennea8@gmail.com>
2023-09-11 16:12:49 +0800

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from "./providers";
-import SidebarWithHeader from "@components/SidebarWithHeader";
+import Layout from "@components/Layout";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,12 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <Providers>
-        <SidebarWithHeader>
-
+        <Layout>
             {children}
-
-        </SidebarWithHeader>
-
+        </Layout>
       </Providers>
       </body>
     </html>
