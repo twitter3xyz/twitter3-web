256499b2e926490b9855733793fc0bc2a7909fac
ennea8 <ennea8@gmail.com>
2023-09-12 12:23:03 +0800

    - Add home hero - Change logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import { Button } from '@chakra-ui/react'
 
+import HomeHero from "@components/HomeHero"
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between gap-10">
 
+      <HomeHero />
     </main>
   );
 }
